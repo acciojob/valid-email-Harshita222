6a1ea3157ac8b09c1f9d226286cd3b05fb4eba04
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-04-26 18:07:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // Test the input string against the pattern and return the result
+    return pattern.test(str);
+
 }
 
 // Do not change the code below.
